aa1c53152a950e745242f25c0e44e740c27581f4
Zfex <55106553+Zfex@users.noreply.github.com>
2024-12-13 15:49:06 +0100

    Add files via upload

---
+++ App/Rover.Domain/Db.cs
@@ -0,0 +1,13 @@
+﻿namespace Rover.Domain
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int PhoneNunber { get; set; }
+        public string Email { get; set; }
+        public string MainCity { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
