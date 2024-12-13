2fe997a8ee28a09786232a7da7244dac8eb65818
Zfex <55106553+Zfex@users.noreply.github.com>
2024-12-13 15:49:55 +0100

    Add files via upload

---
+++ App/Rover.Infrastructure/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Rover.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Rover.Infrastructure
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
