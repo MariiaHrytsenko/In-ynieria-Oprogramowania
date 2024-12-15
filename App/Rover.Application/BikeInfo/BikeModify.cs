c4d3c354c7c0dd7653cbec378834e5696e3ed8db
Zfex <55106553+Zfex@users.noreply.github.com>
2024-12-15 17:30:09 +0100

    Add files via upload

---
+++ App/Rover.Application/BikeInfo/BikeModify.cs
@@ -0,0 +1,40 @@
+﻿using Rover.Domain;
+using Rover.Infrastructure;
+using Rover.Application;
+using MediatR;
+
+namespace Rover.Application.BikeInfo
+{
+    public class BikeModify
+    {
+        public class Command : IRequest<Unit>
+        {
+            public required Bike Bike { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command, Unit>
+        {
+            private readonly DataContext _context;
+
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var bike = await _context.Bikes.FindAsync(request.Bike.BikeId);
+
+                bike.Brand = request.Bike.Brand ?? bike.Brand;
+                bike.Model = request.Bike.Model ?? bike.Model;
+                bike.Type = request.Bike.Type;
+                bike.Price = request.Bike.Price;
+                bike.Stock = request.Bike.Stock;
+
+                await _context.SaveChangesAsync();
+
+                return Unit.Value;
+            }
+        }
+    }
+}
